986e1e4b5990004bb40cb80557f737ec8e6af938
Jack Sheehy <jackhughsheehy@gmail.com>
2023-09-16 00:16:19 +0100

    Text Change

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -9,15 +9,16 @@ function Search() {
   return (
     <>
       <main className="bg-thegray">
-        <div className=" flex flex-col items-start justify-center relative pb-0 px-4 md:px-8 lg:px-16">
-          {/* Changed flex direction to column, items-start will align items to left in this case */}
-          <h1 className="font-Mona font-bold text-white text-5xl leading-20 pb-0 max-w-2xl">
+        <div className="flex flex-col items-start justify-center relative pb-0 px-4 md:px-6 lg:px-8 xl:max-w-4xl mx-auto">
+          {/* Adjusted padding for medium and large screens and set a max-width for extra-large screens */}
+          <h1 className="font-Mona font-bold text-white text-5xl leading-20 pb-0 max-w-xl">
             Search
           </h1>
           <LocationAutosuggest selectedCity={city} onCityChange={setCity} />
         </div>
 
-        <div className=" flex flex-col items-start justify-center relative pb-32 px-4 md:px-8 lg:px-16">
+        <div className="flex flex-col items-start justify-center relative pb-32 px-4 md:px-6 lg:px-8 xl:max-w-4xl mx-auto">
+          {/* Adjusted padding for medium and large screens and set a max-width for extra-large screens */}
           <div className="relative z-1000 pt-0 pb-0">
             <TopGithubUsers city={city} />
           </div>
