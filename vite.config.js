d9cdb5390d87712bfee18fa52f672a9dc6ae0515
Jack Sheehy <jackhughsheehy@gmail.com>
2023-10-04 00:06:31 +0100

    Remove Million

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import million from "million/compiler";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [react()],
   define: {},
 });
