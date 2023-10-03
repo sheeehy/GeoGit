dabed9193e8322b809a3f1579aeb9a65e6938060
Jack Sheehy <jackhughsheehy@gmail.com>
2023-10-04 00:01:49 +0100

    Million v2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
   define: {},
