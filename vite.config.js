9814c0bb839081e2a954b964c541a3099fbbb3dc
Jack Sheehy <jackhughsheehy@gmail.com>
2023-10-10 21:38:05 +0100

    Shadcn/ui

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
