bef6492635c5625218672a627743da2e449a8de5
Jack Sheehy <jackhughsheehy@gmail.com>
2023-10-10 21:47:26 +0100

    Revert "Shadcn/ui"
    This reverts commit 9814c0bb839081e2a954b964c541a3099fbbb3dc.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  define: {},
 });
